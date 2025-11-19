217bf2700d872030db037e45b8a696ddc2f9ecd0
Amar-dev1 <3marofficial249@gmail.com>
2025-11-19 20:54:42 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default {
 	async fetch() {
 		let status = false;
 		console.log('Current status : ', status);
-		return new Response(JSON.stringify({ isPublished: status }), {
+		return new Response(JSON.stringify({ isPublished: status, url: status ? 'https://thikra.netlify.app' : null }), {
 			headers: { 'Content-Type': 'application/json' },
 		});
 	},
