10631d80e1456b12a426eb4df776a4119c3aa794
Amar-dev1 <3marofficial249@gmail.com>
2025-11-19 13:21:33 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	async fetch() {
+		let status = false;
+		console.log('Current status : ', status);
+		return new Response(JSON.stringify({ isPublished: status }), {
+			headers: { 'Content-Type': 'application/json' },
+		});
 	},
 };
