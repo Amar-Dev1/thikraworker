6cf3d18160687e484de179719db5523beb0f4ec3
Amar-dev1 <3marofficial249@gmail.com>
2026-07-23 17:20:20 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ export default {
 	async fetch() {
 		// Set to true to trigger update redirect for old preview users
 		let isPublished = false;
-		
-		return new Response(JSON.stringify({ 
-			isPublished: isPublished, 
-			url: 'https://thikra.netlify.app/new-update',
-			
-			// New properties for the updated app check
-			version: '1.0.4',
-			message: 'تحديث جديد متاح! يرجى التحديث للحصول على أحدث الميزات والتحسينات.'
-		}), {
-			headers: { 
-				'Content-Type': 'application/json',
-				'Access-Control-Allow-Origin': '*'
+
+		if (!isPublished) return;
+
+		return new Response(
+			JSON.stringify({
+				isPublished: isPublished,
+				url: null ,// 'https://thikra.netlify.app/new-update',
+
+				// New properties for the updated app check
+				version: null, // '1.0.4'
+				message: 'تحديث جديد متاح! يرجى التحديث للحصول على أحدث الميزات والتحسينات.',
+			}),
+			{
+				headers: {
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin': '*',
+				},
 			},
-		});
+		);
 	},
 };
