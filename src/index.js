0fd82e566a20ea5b1c31a33f38816df4f992165f
Amar-dev1 <3marofficial249@gmail.com>
2026-08-12 23:45:31 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -37,8 +37,8 @@ export default {
 				}
 
 				const clientVersion = body.version_name || body.version || '0.0.0';
-				const latestVersion = env.LATEST_VERSION || '2.0.2';
-				const bundleUrl = env.BUNDLE_URL || 'https://github.com/Amar-Dev1/thikra-capacitor/releases/download/v2.1.0/bundle-ota.zip';
+				const latestVersion = env.LATEST_VERSION || '2.1.1';
+				const bundleUrl = env.BUNDLE_URL || 'https://github.com/Amar-Dev1/thikra-capacitor/releases/download/v2.1.1/bundle-ota.zip';
 				const checksum = env.CHECKSUM || '';
 
 				// Compare version string
